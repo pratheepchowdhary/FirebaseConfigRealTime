1d9fc8753b449ffc997d5c7285aa8b0053ce31bd
pratheepkanati <39478564+pratheepkanati@users.noreply.github.com>
2020-02-06 16:13:17 +0530

    Intial Commit

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+
+exports.pushConfig = functions.remoteConfig.onUpdate(versionMetadata => {
+    // Create FCM payload to send data message to PUSH_RC topic.
+    const payload = {
+      topic: "PUSH_RC",
+      data: {
+        "CONFIG_STATE": "STALE"
+      }
+    };
+    // Use the Admin SDK to send the ping via FCM.
+    return admin.messaging().send(payload).then(resp => {
+      console.log(resp);
+      return null;
+    });
+  });
